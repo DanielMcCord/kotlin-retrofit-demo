514a3184f2a44ac3b7e7d50abfc0b1a53f334c58
Daniel McCord <git@danielmccord.dev>
2022-03-16 03:24:44 -0700

    Initial commit
    Updated to use Jetpack view binding instead of Kotlin synthetics.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Kotlin Retrofit Demo
+Source: https://www.youtube.com/watch?v=5gFrXGbQsc8
---
+++ app/src/main/java/com/example/kotlinretrofitdemo/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.kotlinretrofitdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.kotlinretrofitdemo.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.StringBuilder
+
+const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        getMyData()
+    }
+
+    private fun getMyData() {
+        val retrofitBuilder = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(ApiInterface::class.java)
+
+        val retrofitData = retrofitBuilder.getData()
+
+        retrofitData.enqueue(object : Callback<List<MyDataItem>?> {
+            override fun onResponse(
+                call: Call<List<MyDataItem>?>,
+                response: Response<List<MyDataItem>?>
+            ) {
+                val responseBody = response.body()!!
+
+                val myStringBuilder = StringBuilder()
+                for (myData in responseBody) {
+                    myStringBuilder.append(myData.id)
+                    myStringBuilder.append("\n")
+                }
+
+                // See https://developer.android.com/topic/libraries/view-binding#activities
+                binding.txtId.text = myStringBuilder
+            }
+
+            override fun onFailure(call: Call<List<MyDataItem>?>, t: Throwable) {
+                Log.d("MainActivity", "onFailure: " + t.message)
+            }
+        })
+    }
+}
